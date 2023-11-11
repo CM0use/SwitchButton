8afaa4d88b4b15b99eb3420c20d1706f7ef1fb32
CM0use <dilanuzcs@gmail.com>
2023-11-11 08:38:44 -0400

    Add files via upload

---
+++ src/SwitchButton.pro
@@ -0,0 +1,23 @@
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++17
+
+DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+VERSION = 1.0.0
+QMAKE_TARGET_PRODUCT = SwitchButton
+QMAKE_TARGET_COPYRIGHT = CM0use
+
+SOURCES += \
+    main.cpp \
+    switchbutton.cpp
+
+HEADERS += \
+    switchbutton.h
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+// SwitchButton
+// Copyright (c) 2023 CM0use
+
+#include "switchbutton.h"
+
+#include <QApplication>
+#include <QButtonGroup>
+#include <QGridLayout>
+
+#define THEME (false)
+
+int main(int argc, char *argv[])
+{
+    QApplication application{ argc, argv };
+
+    application.setStyleSheet(THEME? "QWidget { background-color: #292a2d; color: #E8EAED }"
+                                    : "QWidget { background-color: #FFF; color: #202124 }");
+
+    SwitchButton *switch1{ new SwitchButton("SWITCH", THEME) };
+    switch1->setChecked(true);
+    SwitchButton* switch2{ new SwitchButton("SWITCH", THEME) };
+    switch2->setDisabled(true);
+    SwitchButton *switch3{ new SwitchButton("SWITCH", THEME) };
+    switch3->setLayoutDirection(Qt::RightToLeft);
+    SwitchButton *switch4{ new SwitchButton("SWITCH", THEME) };
+    switch4->setLayoutDirection(Qt::RightToLeft);
+    switch4->setChecked(true);
+    switch4->setDisabled(true);
+
+    SwitchButton *item1{ new SwitchButton("ITEM1", THEME) };
+    SwitchButton *item2{ new SwitchButton("ITEM2", THEME) };
+    QButtonGroup bg;
+    bg.addButton(item1);
+    bg.addButton(item2);
+
+    QGridLayout *mainLayout{ new QGridLayout };
+    mainLayout->addWidget(switch1, 0, 0, Qt::AlignCenter);
+    mainLayout->addWidget(switch2);
+    mainLayout->addWidget(switch3);
+    mainLayout->addWidget(switch4);
+    mainLayout->addWidget(item1);
+    mainLayout->addWidget(item2);
+
+    QWidget *window{ new QWidget };
+    window->setLayout(mainLayout);
+    window->show();
+
+    return application.exec();
+}
